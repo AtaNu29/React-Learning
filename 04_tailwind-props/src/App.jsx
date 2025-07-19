e1ed4396c25e5ba10f9cc78d967ad0393bb416dc
AtaNu29 <atanumondal0017@gmail.com>
2025-07-19 18:34:10 +0530

    add props projects

---
+++ 04_tailwind-props/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myObj = {
+    name: 'John',
+    age: 30
+  }
+
+  let myArr = [1, 2, 3, 4, 5]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-2 rounded-2xl font-bold'>Tailwind Test</h1>
+      <Card src='https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg' userName='Atanu Mondal' cardValue= {345}/>
+      <Card src='https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif' userName='Pinaki Mukharjee' price = {100} cardValue= {221}/>
+    </>
+  )
+}
+
+export default App
---
+++ 04_tailwind-props/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function card({src, userName, price = 50, cardValue}) {
+  return (
+    <div className="w-60 flex flex-col rounded-xl bg-black min-h-[19rem] mt-3">
+      <div>
+        <img
+          src={src}
+          alt="test"
+          className="object-cover object-center rounded-t-xl"
+        />
+      </div>
+      <div className="flex flex-col py-3 px-3 pb-10">
+        <div className="flex justify-between ">
+          <h1 className="font-bold ">{userName}</h1>
+          <h1>Price</h1>
+        </div>
+        <div className="flex  justify-between">
+          <p>#{cardValue}</p>
+          <p>₹ {price}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default card
